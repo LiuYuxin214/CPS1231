351a699a4973ebdaf217910796933cd954cf15a7
LiuYuxin214 <13791114010@163.com>
2021-11-17 13:50:19 +0800

    LiuYuxin

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 public class GuessNumber {
 
     public static void main(String[] args) {
-        //1. Show tips
+        //1. Show tips and generates an integer between 0 and 100
         System.out.println("Guess a magic number from 0 to 100");
         System.out.println("");
+        int num = (int) (Math.random() * 101);
 
         //2. Get guess value from user
-        boolean mark = false;
-        while(mark == false) {
-            int num1 = (int) (Math.random() * 101);
+        int guess = 101;
+        while(guess != num) {
             System.out.print("Enter your guess: ");
             Scanner input = new Scanner(System.in);
-            int guess = input.nextInt();
+            guess = input.nextInt();
 
             //3. Judge the result of speculation
-            if (guess > num1) System.out.println("Your guess is too high");
-            else if (guess < num1) System.out.println("Your guess is too low");
-            else if (guess == num1) {
-                System.out.println("The number is: " + num1);
-                mark = true;
+            if (guess > num) System.out.println("Your guess is too high");
+            else if (guess < num) System.out.println("Your guess is too low");
+            else if (guess == num) {
+                System.out.println("The right number is: " + num);
             }
         }
     }
--- src/GuessNumberDoWhile.java
+++ src/GuessNumberDoWhile.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class GuessNumberDoWhile {
 
     public static void main(String[] args) {
-        //1. Show tips
+        //1. Show tips and generates an integer between 0 and 100
         System.out.println("Guess a magic number from 0 to 100");
         System.out.println("");
+        int num = (int) (Math.random() * 101);
 
         //2. Get guess value from user
-        boolean mark = false;
+        int guess=101;
          do{
-            int num1 = (int) (Math.random() * 101);
             System.out.print("Enter your guess: ");
             Scanner input = new Scanner(System.in);
-            int guess = input.nextInt();
+            guess = input.nextInt();
 
             //3. Judge the result of speculation
-            if (guess > num1) System.out.println("Your guess is too high");
-            else if (guess < num1) System.out.println("Your guess is too low");
-            else if (guess == num1) {
-                System.out.println("The number is: " + num1);
-                mark = true;
+            if (guess > num) System.out.println("Your guess is too high");
+            else if (guess < num) System.out.println("Your guess is too low");
+            else if (guess == num) {
+                System.out.println("The right number is: " + num);
             }
-        }while(mark == false);
+         }while(guess != num);
     }
 }
