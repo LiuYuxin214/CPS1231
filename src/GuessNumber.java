04e15862a55921d90e8f864771bcd137f4f228e0
LiuYuxin214 <13791114010@163.com>
2021-11-17 13:24:45 +0800

    LiuYuxin

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-//Purpose:
-//Main idea:
+//Purpose: randomly generates an integer between 0 and 100 and prompt the user to enter a number, and
+//print corresponding message to user
+//Main idea: Math.random() while loop
 public class GuessNumber {
 
     public static void main(String[] args) {
-        //1.
+        //1. Show tips
         System.out.println("Guess a magic number from 0 to 100");
         System.out.println("");
 
-        //2.
+        //2. Get guess value from user
         boolean mark = false;
         while(mark == false) {
             int num1 = (int) (Math.random() * 101);
@@ -16,7 +17,7 @@ public class GuessNumber {
             Scanner input = new Scanner(System.in);
             int guess = input.nextInt();
 
-            //3.
+            //3. Judge the result of speculation
             if (guess > num1) System.out.println("Your guess is too high");
             else if (guess < num1) System.out.println("Your guess is too low");
             else if (guess == num1) {
