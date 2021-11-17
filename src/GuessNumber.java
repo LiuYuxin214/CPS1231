7faea8fbf089c13cc2f93af1f524ae78be2eb592
LiuYuxin214 <13791114010@163.com>
2021-11-17 13:14:43 +0800

    LiuYuxin

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
+//Purpose:
+//Main idea:
 public class GuessNumber {
 
     public static void main(String[] args) {
+        //1.
         System.out.println("Guess a magic number from 0 to 100");
         System.out.println("");
 
-        boolean x = false;
-        while(x == false) {
-            double num = Math.random() * 101;
-            int num1 = (int) num;
+        //2.
+        boolean mark = false;
+        while(mark == false) {
+            int num1 = (int) (Math.random() * 101);
             System.out.print("Enter your guess: ");
             Scanner input = new Scanner(System.in);
             int guess = input.nextInt();
 
+            //3.
             if (guess > num1) System.out.println("Your guess is too high");
             else if (guess < num1) System.out.println("Your guess is too low");
             else if (guess == num1) {
                 System.out.println(num1);
-                x = true;
+                mark = true;
             }
         }
     }
-
 }
