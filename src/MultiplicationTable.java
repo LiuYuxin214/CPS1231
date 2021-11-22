15713379890c0cd5bc195fd40f99de1306ca1e04
LiuYuxin214 <13791114010@163.com>
2021-11-22 14:51:43 +0800

    LiuYuxin

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,25 @@
+//Purpose:
+//Main idea: 
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        //1.
+        System.out.print("   ");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(" " + i + " ");
+        }
+        System.out.println("");
+        System.out.print("-----------------------------");
+        System.out.println("");
+        for(int i = 1; i <= 9; i++){
+            System.out.print(i+"| ");
+            for(int j = 1; j <= 9; j++){
+                if(i*j<=9) System.out.print(" " + i * j + " ");
+                else System.out.print(i*j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
